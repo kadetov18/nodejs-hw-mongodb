7c33735932888ebd32f9cd9b5425d1e4cc06c083
kadetov18 <kadetovolegosnovnai@gmail.com>
2024-10-01 16:54:54 +0300

    hw-4-valid_ref

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,32 +7,21 @@ export const validationBody = Joi.object({
     'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number should have at least {#limit} characters',
+    'string.max': 'Phone number should have at most {#limit} characters',
+    'any.required': 'Phone number is required',
+  }),
   email: Joi.string().email(),
-  phoneNumber: Joi.string().min(5).max(17),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
   isFavourite: Joi.boolean(),
 });
 
-const bodyToVAlidate = {
-  name: 'John Doe',
-  email: 'john.doe@example.com',
-  phoneNumber: '0000000000',
-  isFavourite: false,
-};
-
-const validationResult = validationBody.validate(bodyToVAlidate, {
-  abortEarly: false,
-});
-if (validationResult.error) {
-  console.error(validationResult.error.message);
-} else {
-  console.log(`GG WP`);
-}
-
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+  isFavourite: Joi.boolean(),
 });
